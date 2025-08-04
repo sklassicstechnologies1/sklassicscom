5594dce210bc166f9da6b7970e83f78c74e9a0dd
Preethi Diddi <diddipreethi@gmail.com>
2025-08-04 18:57:52 +0530

    file name imported correctly

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Rating from './Rating'
 import Us from './Us'
-import CourseHome from './CourseHome.JSX'
+
 import ContactHome from './ContactHome'
+import CourseHome from './CourseHome'
 
 
 const Home = () => {
